66bed5c3f8010682ffa6c031f03e9888ef5b21ed
Pawel Drozdowski <p.drozdowski@vp.pl>
2013-03-18 19:44:02 +0100

    adding game mode impact on game end conditions

--- Infusion/Gaming/LightCycles/LightCyclesGame.cs
+++ Infusion/Gaming/LightCycles/LightCyclesGame.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace Infusion.Gaming.LightCycles
 {
+    using System;
     using Infusion.Gaming.LightCycles.Conditions;
     using Infusion.Gaming.LightCycles.Events.Filtering;
     using Infusion.Gaming.LightCycles.Events.Processing;
@@ -17,7 +18,7 @@ namespace Infusion.Gaming.LightCycles
         /// Starts game with random map
         /// </summary>
         /// <param name="numberOfPlayers">number of players in the game</param>
-        public void StartOnRandomMap(int numberOfPlayers)
+        public void StartOnRandomMap(int numberOfPlayers, GameModeEnum gameMode)
         {
             // init
             var generator = new MapStreamGenerator();
@@ -26,31 +27,41 @@ namespace Infusion.Gaming.LightCycles
             var mapSerializer = new MapSerializer();
             IMap map = mapSerializer.Read(mapStream);
 
+            EndConditionSet endConditions = new EndConditionSet();
+            endConditions.Add(new EndCondition(new NumberOfPlayers(0), GameResultEnum.FinishedWithoutWinner));
+
+            switch (gameMode)
+            {
+                case GameModeEnum.FreeForAll:
+                    endConditions.Add(new EndCondition(new NumberOfPlayers(1), GameResultEnum.FinshedWithWinner));
+                    break;
+                case GameModeEnum.TeamDeathmatch:
+                    endConditions.Add(new EndCondition(new NumberOfTeams(1), GameResultEnum.FinshedWithWinners));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("gameMode");
+            }
+
+            EventFilterSet eventFilters = new EventFilterSet();
+            eventFilters.Add(new PlayersInGameFilter());
+            eventFilters.Add(new PlayerRecentEventFilter());
+            eventFilters.Add(new IdlePlayerMoveEventAppender(RelativeDirectionEnum.Undefined));
+
+            EventProcessorSet eventProcesors = new EventProcessorSet();
+            eventProcesors.Add(new EventLoggingProcessor(true));
+            eventProcesors.Add(new PlayerMovesProcessor());
+            eventProcesors.Add(new PlayerCollisionProcessor());
+            eventProcesors.Add(new TrailAgingProcessor(0.2f));
+            eventProcesors.Add(new GarbageProcessor(true));
+
             // start
             this.Start(
                 GameModeEnum.FreeForAll,
                 map.Players,
                 map,
-                new EndConditionSet
-                        {
-                            new EndCondition(new NumberOfPlayers(0), GameResultEnum.FinishedWithoutWinner),
-                            new EndCondition(new NumberOfPlayers(1), GameResultEnum.FinshedWithWinner),
-                            new EndCondition(new NumberOfTeams(1), GameResultEnum.FinshedWithWinners),
-                        },
-                new EventFilterSet
-                        {
-                            new PlayersInGameFilter(),
-                            new PlayerRecentEventFilter(),
-                            new IdlePlayerMoveEventAppender(RelativeDirectionEnum.Undefined)
-                        },
-                new EventProcessorSet
-                        {
-                            new EventLoggingProcessor(true),
-                            new PlayerMovesProcessor(),
-                            new PlayerCollisionProcessor(),
-                            new TrailAgingProcessor(0.2f),
-                            new GarbageProcessor(true)
-                        });
+                endConditions,
+                eventFilters,
+                eventProcesors);
         }
     }
 }
