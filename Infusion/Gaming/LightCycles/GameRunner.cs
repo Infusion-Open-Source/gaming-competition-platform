a49ebf167224d90166cba6a2245630439b389f8d
Pawel Drozdowski <p.drozdowski@vp.pl>
2013-04-05 16:05:41 +0200

    added new bot, bots factory, and basic game runner class

---
+++ Infusion/Gaming/LightCycles/GameRunner.cs
@@ -0,0 +1,115 @@
+﻿namespace Infusion.Gaming.LightCycles
+{
+    using System;
+    using System.Collections.Generic;
+    using Infusion.Gaming.LightCycles.Bots;
+    using Infusion.Gaming.LightCycles.Events;
+    using Infusion.Gaming.LightCycles.Model.Defines;
+    using Infusion.Gaming.LightCycles.Model.Serialization;
+
+    /// <summary>
+    /// Runs game in a loop
+    /// </summary>
+    public class GameRunner
+    {
+        /// <summary>
+        /// List of AI players
+        /// </summary>
+        private readonly List<IBot> bots = new List<IBot>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameRunner" /> class.
+        /// </summary>
+        public GameRunner()
+        {
+            this.ConsoleOutputEnabled = true;
+        }
+
+        /// <summary>
+        /// Gets or sets game main object
+        /// </summary>
+        public LightCyclesGame Game { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether console output is enabled
+        /// </summary>
+        public bool ConsoleOutputEnabled { get; set; }
+        
+        /// <summary>
+        /// Start a game
+        /// </summary>
+        /// <param name="numberOfPlayers">number of players in the game</param>
+        /// <param name="gameMode">game mode to be played</param>
+        public void StartGame(int numberOfPlayers, GameModeEnum gameMode)
+        {
+            var botsFactory = new BotFactory();
+            this.Game = new LightCyclesGame();
+            this.Game.StartOnRandomMap(numberOfPlayers, gameMode);
+            foreach (var player in this.Game.CurrentState.PlayersData.Players)
+            {
+                this.bots.Add(botsFactory.CreateBot(player));
+            }
+
+            this.ShowGameState();
+        }
+
+        /// <summary>
+        /// Run a game step
+        /// </summary>
+        /// <returns>Is game still running</returns>
+        public bool RunGame()
+        {
+            if (this.Game.State == GameStateEnum.Running)
+            {
+                var events = new List<Event>();
+                foreach (var bot in this.bots)
+                {
+                    events.AddRange(bot.GetPlayerEvents(this.Game.CurrentState));
+                }
+
+                this.Game.Step(events);
+                this.ShowGameState();
+            }
+
+            return this.Game.State == GameStateEnum.Running;
+        }
+
+        /// <summary>
+        /// End the game
+        /// </summary>
+        public void EndGame()
+        {
+            this.ShowGameResultMessage();
+        }
+        
+        /// <summary>
+        /// Show game current state on a console
+        /// </summary>
+        public void ShowGameState()
+        {
+            new GameStateRenderer().Render(this.Game.CurrentState);
+        }
+
+        /// <summary>
+        /// Shows game result message on a console
+        /// </summary>
+        public void ShowGameResultMessage()
+        {
+            switch (this.Game.Result)
+            {
+                case GameResultEnum.FinshedWithWinners:
+                    Console.WriteLine("End, winning team is: " + this.Game.CurrentState.PlayersData.Players[0].Team.Id);
+                    break;
+                case GameResultEnum.FinshedWithWinner:
+                    Console.WriteLine("End, winner is: " + this.Game.CurrentState.PlayersData.Players[0]);
+                    break;
+                case GameResultEnum.FinishedWithoutWinner:
+                    Console.WriteLine("End, no winner");
+                    break;
+                case GameResultEnum.Terminated:
+                    Console.WriteLine("Terminated");
+                    break;
+            }
+        }
+    }
+}
--- Infusion/Gaming/ServerLauncher/Program.cs
+++ Infusion/Gaming/ServerLauncher/Program.cs
@@ -1,133 +1,14 @@
 ﻿namespace Infusion.Gaming.ServerLauncher
 {
     using System;
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Threading;
     using Infusion.Gaming.LightCycles;
-    using Infusion.Gaming.LightCycles.Events;
-    using Infusion.Gaming.LightCycles.Model;
-    using Infusion.Gaming.LightCycles.Model.Data;
     using Infusion.Gaming.LightCycles.Model.Defines;
-    using Infusion.Gaming.LightCycles.Model.Serialization;
 
     /// <summary>
     /// The program.
     /// </summary>
     internal class Program
     {
-        /// <summary>
-        /// The random number generator.
-        /// </summary>
-        private static readonly Random Random = new Random(1);
-
-        /// <summary>
-        /// The next random direction.
-        /// </summary>
-        /// <param name="player">
-        /// Player for which takes next direction
-        /// </param>
-        /// <param name="state">
-        /// state of the game
-        /// </param>
-        /// <returns>
-        /// The random direction <see cref="RelativeDirectionEnum"/>.
-        /// </returns>
-        public static RelativeDirectionEnum NextDirection(Player player, IGameState state)
-        {
-            if (!state.PlayersData.Players.Contains(player))
-            {
-                return RelativeDirectionEnum.Undefined;
-            }
-
-            Point location = state.PlayersData.PlayersLocations[player];
-            DirectionEnum direction = state.PlayersData.PlayersLightCycles[player].Direction;
-
-            // get possible directions
-            var safeDirections = new List<RelativeDirectionEnum>();
-            safeDirections.Add(RelativeDirectionEnum.Right);
-            safeDirections.Add(RelativeDirectionEnum.Left);
-            safeDirections.Add(RelativeDirectionEnum.StraightForward);
-
-            // remove unsafe
-            for (int i = 0; i < safeDirections.Count; i++)
-            {
-                DirectionEnum newDirection = DirectionHelper.ChangeDirection(direction, safeDirections[i]);
-                Point newLocation = DirectionHelper.NextLocation(location, newDirection);
-                if (!state.Map[newLocation.X, newLocation.Y].IsPassable || state.PlayersData[newLocation.X, newLocation.Y] != null)
-                {
-                    safeDirections.RemoveAt(i--);
-                }
-            }
-
-            // is no way to go, go straight ahead
-            if (safeDirections.Count == 0)
-            {
-                return RelativeDirectionEnum.StraightForward;
-            }
-
-            // if can go streight, go streight - makes it look more fancy ;)
-            if (safeDirections.Contains(RelativeDirectionEnum.StraightForward))
-            {
-                return RelativeDirectionEnum.StraightForward;
-            }
-
-            // pick one randomly 
-            return safeDirections[Random.Next(safeDirections.Count)];
-        }
-
-        /// <summary>
-        /// Plays random game.
-        /// </summary>
-        public static void PlayRandomGame()
-        {
-            // init
-            const int NumberOfPlayers = 8;
-            var game = new LightCyclesGame();
-            var stateRenderer = new GameStateRenderer();
-            game.StartOnRandomMap(NumberOfPlayers, GameModeEnum.FreeForAll);
-            
-            // do initial UI
-            stateRenderer.Render(game.CurrentState);
-            while (game.State == GameStateEnum.Running)
-            {
-                // tick
-                var events = new List<Event>();
-                for (int p = 0; p < NumberOfPlayers; p++)
-                {
-                    var player = new Player((char)('A' + p));
-                    events.Add(new PlayerMoveEvent(player, NextDirection(player, game.CurrentState)));
-                }
-
-                game.Step(events);
-
-                // do the UI
-                stateRenderer.Render(game.CurrentState);
-                Thread.Sleep(100);
-            }
-
-            // end
-            if (game.Result == GameResultEnum.FinshedWithWinners)
-            {
-                Console.WriteLine("End, winning team is: " + game.CurrentState.PlayersData.Players[0].Team.Id);
-            }
-
-            if (game.Result == GameResultEnum.FinshedWithWinner)
-            {
-                Console.WriteLine("End, winner is: " + game.CurrentState.PlayersData.Players[0]);
-            }
-
-            if (game.Result == GameResultEnum.FinishedWithoutWinner)
-            {
-                Console.WriteLine("End, no winner");
-            }
-
-            if (game.Result == GameResultEnum.Terminated)
-            {
-                Console.WriteLine("Terminated");
-            }
-        }
-
         /// <summary>
         /// Program main method.
         /// </summary>
@@ -140,9 +21,19 @@
             Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY; for details check License.txt file.");
             Console.WriteLine("This is free software, and you are welcome to redistribute it under certain conditions; check License.txt file for the details.");
 
+            const int NumberOfPlayers = 8;
+            const GameModeEnum GameMode = GameModeEnum.FreeForAll;
+
             while (true)
             {
-                PlayRandomGame();
+                var gameRunner = new GameRunner();
+                gameRunner.StartGame(NumberOfPlayers, GameMode);
+                while (gameRunner.RunGame())
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                gameRunner.EndGame();
                 Console.ReadKey();
             }
         }
